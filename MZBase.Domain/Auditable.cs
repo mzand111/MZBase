52ff55ffd5afb8e13257971b69c06975e3afa9bb
Mahdi <m.zandakbari@gmail.com>
2022-07-05 18:01:25 +0430

    Added IAuditables

---
+++ MZBase.Domain/Auditable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MZBase.Domain
+{
+    public class Auditable<PrimaryKey> : IAuditable<PrimaryKey>
+        where PrimaryKey : struct
+    {
+        public string CreatedBy { get; set; }
+        public string LastModifiedBy { get; set ; }
+        public DateTime CreationTime { get; set; }
+        public DateTime LastModificationTime { get; set; }
+        public PrimaryKey ID { get; set; }
+    }
+}
---
+++ MZBase.Domain/IAuditable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MZBase.Domain
+{
+    public interface IAuditable<PrimaryKey>:IModel<PrimaryKey>
+        where PrimaryKey : struct
+    {
+        string CreatedBy { get; set; }
+        string LastModifiedBy { get; set; }
+        DateTime CreationTime { get; set; }
+        DateTime LastModificationTime { get; set; }
+    }
+}
