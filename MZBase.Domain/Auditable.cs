b3d61eaac4b1ef2d10919e678795d6c2f4e005e7
saber <saeid.saber.k@gmail.com>
2022-09-18 18:12:03 +0430

    Add Column Order DataAnnotation

--- MZBase.Domain/Auditable.cs
+++ MZBase.Domain/Auditable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace MZBase.Domain
@@ -7,10 +8,15 @@ namespace MZBase.Domain
     public class Auditable<PrimaryKey> : Model<PrimaryKey>,IAuditable<PrimaryKey>
         where PrimaryKey : struct
     {
+        [Column(Order = 100)]
         public virtual string CreatedBy { get; set; }
+        [Column(Order = 101)]
         public virtual string LastModifiedBy { get; set ; }
+        [Column(Order = 102)]
         public virtual DateTime CreationTime { get; set; }
+        [Column(Order = 103)]
         public virtual DateTime LastModificationTime { get; set; }
+        [Column(Order =0)]
         public virtual PrimaryKey ID { get; set; }
     }
 }
