12b82b57ba6eaee643f36e96c7444430f002450b
Mahdi Zand <80402823+mzand111@users.noreply.github.com>
2025-03-04 16:17:11 +0300

    Added SetAuditablesFrom

--- MZBase.Domain/Auditable.cs
+++ MZBase.Domain/Auditable.cs
@@ -1,21 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
 
 namespace MZBase.Domain
 {
-    public class Auditable<PrimaryKey> : Model<PrimaryKey>,IAuditable<PrimaryKey>
+    public class Auditable<PrimaryKey> : Model<PrimaryKey>, IAuditable<PrimaryKey>
         where PrimaryKey : struct
     {
         [Column(Order = 100)]
         public virtual string CreatedBy { get; set; }
         [Column(Order = 101)]
-        public virtual string LastModifiedBy { get; set ; }
+        public virtual string LastModifiedBy { get; set; }
         [Column(Order = 102)]
         public virtual DateTime CreationTime { get; set; }
         [Column(Order = 103)]
         public virtual DateTime LastModificationTime { get; set; }
-     
+
+        public void SetAuditablesFrom<T>(T t)
+            where T : Auditable<PrimaryKey>
+        {
+            CreatedBy = t.CreatedBy;
+            CreationTime = t.CreationTime;
+            LastModificationTime = t.LastModificationTime;
+            LastModifiedBy = t.LastModifiedBy;
+        }
+
     }
 }
